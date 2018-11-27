4924a8bb5fd9d7cddd5d4a1246b9f3d732427b16
Aidan Feldman <aidan.l.feldman@census.gov>
2018-11-27 14:33:17 -0500

    take the source file and table name as arguments

--- import.py
+++ import.py
@@ -1,10 +1,13 @@
+import argparse
 import sqlite3
 from sas7bdat import SAS7BDAT
 
 
-FILES = {
-    'ssocs': 'ssocs.sas7bdat',
-}
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('src', help='path to the source *.sas7bdat file')
+parser.add_argument('table', help='name of the destination table')
+
+args = parser.parse_args()
 
 con = sqlite3.connect('joined-from-sas.db')
 
@@ -13,19 +16,17 @@ def columns_from_fields(fields):
     return ', '.join(col + " string" for col in fields)
 
 
-for table, path in FILES.items():
-    print("Importing", table, "...", flush=True)
-    with SAS7BDAT(path, skip_header=True) as reader:
-        column_names = [col.decode('utf-8') for col in reader.column_names]
-        columns = columns_from_fields(column_names)
+with SAS7BDAT(args.src, skip_header=True) as reader:
+    column_names = [col.decode('utf-8') for col in reader.column_names]
+    columns = columns_from_fields(column_names)
+    with con:
+        con.execute("DROP TABLE IF EXISTS " + args.table)
+        con.execute("CREATE TABLE {} ({})".format(args.table, columns))
+
+    # continue
+
+    for row in reader:
         with con:
-            con.execute("DROP TABLE IF EXISTS " + table)
-            con.execute("CREATE TABLE {} ({})".format(table, columns))
-
-        # continue
-
-        for row in reader:
-            with con:
-                col_placeholders = ', '.join('?' for key in column_names)
-                con.execute("INSERT INTO {} ({}) VALUES ({})".format(
-                    table, ', '.join(column_names), col_placeholders), tuple(row))
+            col_placeholders = ', '.join('?' for key in column_names)
+            con.execute("INSERT INTO {} ({}) VALUES ({})".format(
+                args.table, ', '.join(column_names), col_placeholders), tuple(row))
