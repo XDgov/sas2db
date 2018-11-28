4fd501ad6615d5c98d3975a8dab3e4565b0135cc
Aidan Feldman <aidan.l.feldman@census.gov>
2018-11-28 14:54:40 -0500

    derive the default file and table names

--- import.py
+++ import.py
@@ -1,18 +1,18 @@
 import argparse
 import sqlite3
+from pathlib import Path
 from sas7bdat import SAS7BDAT
 
 
 parser = argparse.ArgumentParser(description='Import SAS data into a SQLite3 table.',
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('src', help='path to the source *.sas7bdat file')
-parser.add_argument('table', help='name of the destination table')
-parser.add_argument('--db', default='sas.db',
-                    help='name of the SQLite file to use/create')
+parser.add_argument(
+    '--db', help='name of the SQLite file to use/create - defaults to name of the SAS file')
+parser.add_argument(
+    '--table', help='name of the destination table - defaults to the dataset name from the SAS file')
 
 args = parser.parse_args()
-
-con = sqlite3.connect(args.db)
 
 
 def columns_from_fields(fields):
@@ -20,18 +20,24 @@ def columns_from_fields(fields):
 
 
 with SAS7BDAT(args.src, skip_header=True) as reader:
+    db = args.db or Path(args.src).stem + '.db'
+    con = sqlite3.connect(db)
+    dataset_name = reader.properties.name.decode('utf-8')
+    table = args.table or dataset_name
+
+    print("Writing {} rows to {} table in {}...".format(
+        reader.properties.row_count, table, db))
+
     column_names = [col.decode('utf-8') for col in reader.column_names]
     columns = columns_from_fields(column_names)
     with con:
-        con.execute("DROP TABLE IF EXISTS " + args.table)
-        con.execute("CREATE TABLE {} ({})".format(args.table, columns))
+        con.execute("DROP TABLE IF EXISTS " + table)
+        con.execute("CREATE TABLE {} ({})".format(table, columns))
 
-    num_obs = 0
     for row in reader:
         with con:
             col_placeholders = ', '.join('?' for key in column_names)
             con.execute("INSERT INTO {} ({}) VALUES ({})".format(
-                args.table, ', '.join(column_names), col_placeholders), tuple(row))
-            num_obs += 1
+                table, ', '.join(column_names), col_placeholders), tuple(row))
 
-    print(num_obs, "observations written to", args.db)
+    print("Done")
