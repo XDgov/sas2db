6a645a4ab0891c8e1fcc9a12cc550cf2a05053d3
Aidan Feldman <aidan.l.feldman@census.gov>
2018-11-29 11:40:07 -0500

    split importer into smaller functions

--- import.py
+++ import.py
@@ -4,40 +4,54 @@ from pathlib import Path
 from sas7bdat import SAS7BDAT
 
 
-parser = argparse.ArgumentParser(description='Import SAS data into a SQLite3 table.',
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('src', help='path to the source *.sas7bdat file')
-parser.add_argument(
-    '--db', help='name of the SQLite file to use/create - defaults to name of the SAS file')
-parser.add_argument(
-    '--table', help='name of the destination table - defaults to the dataset name from the SAS file')
+def get_args():
+    parser = argparse.ArgumentParser(description='Import SAS data into a SQLite3 table.',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('src', help='path to the source *.sas7bdat file')
+    parser.add_argument(
+        '--db', help='name of the SQLite file to use/create - defaults to name of the SAS file')
+    parser.add_argument(
+        '--table', help='name of the destination table - defaults to the dataset name from the SAS file')
 
-args = parser.parse_args()
+    return parser.parse_args()
 
 
 def columns_from_fields(fields):
     return ', '.join(col + " string" for col in fields)
 
 
-with SAS7BDAT(args.src, skip_header=True) as reader:
-    db = args.db or Path(args.src).stem + '.db'
-    con = sqlite3.connect(db)
-    dataset_name = reader.properties.name.decode('utf-8')
-    table = args.table or dataset_name
-
-    print("Writing {} rows to {} table in {}...".format(
-        reader.properties.row_count, table, db))
-
-    column_names = [col.decode('utf-8') for col in reader.column_names]
+def create_table(con, name, column_names):
     columns = columns_from_fields(column_names)
     with con:
-        con.execute("DROP TABLE IF EXISTS " + table)
-        con.execute("CREATE TABLE {} ({})".format(table, columns))
+        con.execute("DROP TABLE IF EXISTS " + name)
+        con.execute("CREATE TABLE {} ({})".format(name, columns))
 
+
+def write_rows(con, table, column_names, reader):
     for row in reader:
         with con:
             col_placeholders = ', '.join('?' for key in column_names)
             con.execute("INSERT INTO {} ({}) VALUES ({})".format(
                 table, ', '.join(column_names), col_placeholders), tuple(row))
 
-    print("Done")
+
+def run_import(src, db, table):
+    with SAS7BDAT(src, skip_header=True) as reader:
+        db = db or Path(src).stem + '.db'
+        con = sqlite3.connect(db)
+        dataset_name = reader.properties.name.decode('utf-8')
+        table = table or dataset_name
+
+        print("Writing {} rows to {} table in {}...".format(
+            reader.properties.row_count, table, db))
+
+        column_names = [col.decode('utf-8') for col in reader.column_names]
+        create_table(con, table, column_names)
+        write_rows(con, table, column_names, reader)
+
+        print("Done")
+
+
+if __name__ == '__main__':
+    args = get_args()
+    run_import(args.src, args.db, args.table)
