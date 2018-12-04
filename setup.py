c48fcfae0592637d12883b2367d7bd118dc2d06d
Aidan Feldman <aidan.l.feldman@census.gov>
2018-12-04 16:01:30 -0500

    bump to v0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sas2db",
-    version="0.1.0",
+    version="0.2.0",
     author="Aidan Feldman",
     author_email="aidan.l.feldman@census.gov",
     description="Import SAS files to SQL databases",
